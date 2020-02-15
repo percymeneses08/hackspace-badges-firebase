390c7f872d299ddd9b6b280d4719fc1b2bed454c
percymeneses08 <percymeneses08@gmail.com>
2020-02-15 00:46:19 -0500

    Updates

---
+++ precache-manifest.36e4c165f0847bfba31b324423f69d87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf6c313bef5b77ffe952e18f9dff0f7",
+    "url": "/hackspace-badges.firebase/index.html"
+  },
+  {
+    "revision": "4625a47d913c44a57462",
+    "url": "/hackspace-badges.firebase/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "189611076b6f72fc4f59",
+    "url": "/hackspace-badges.firebase/static/js/2.85227447.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hackspace-badges.firebase/static/js/2.85227447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4625a47d913c44a57462",
+    "url": "/hackspace-badges.firebase/static/js/main.ae362e9c.chunk.js"
+  },
+  {
+    "revision": "f249c57edf8b6cc88e5b",
+    "url": "/hackspace-badges.firebase/static/js/runtime-main.8340e0d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hackspace-badges.firebase/static/media/logo.5d5d9eef.svg"
+  }
+]);
